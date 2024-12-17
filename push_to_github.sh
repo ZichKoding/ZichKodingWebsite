1842f57e60bcf1bd6ecdcd401b57d178395ceabf
ZichKoding <chriszichkocoding@gmail.com>
2024-12-16 19:00:05 -0500

    Added a markdown editor to the Projects admin view.

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Initialize the variables for commit message and branch name.
-commit_message="Was able to get the home view created and tested. It needs to be updated within the html file."
+commit_message="Added a markdown editor to the Projects admin view."
 branch_name="main"
 
 # Function to run tests
