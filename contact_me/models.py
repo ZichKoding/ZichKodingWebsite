1b2899176644be6a8179582a0ccf839c62d41ead
ZichKoding <chriszichkocoding@gmail.com>
2025-01-12 16:30:30 -0500

    Updating the Contact Me app to be a one-to-many relationship with Contact-Messages models where Messages.contact_id is the information for the contact its related to.

--- contact_me/models.py
+++ contact_me/models.py
@@ -5,7 +5,16 @@ class Contact(models.Model):
     first_name = models.CharField(blank=True, max_length=50)
     last_name = models.CharField(blank=True, max_length=50)
     email = models.EmailField(unique=True, max_length=255)
+
+    def __str__(self):
+        return self.id
+    
+
+class Message(models.Model):
     message = models.TextField(max_length=1000)
     project_url = models.URLField()
     project_title = models.CharField(blank=True, max_length=100)
-    
+    contact_id = models.ForeignKey(Contact, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.project_title, self.project_url, self.contact_id
--- contact_me/tests/test_models.py
+++ contact_me/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from contact_me.models import Contact 
+from contact_me.models import Contact, Message
 
 
 class TestContactModel(TestCase):
@@ -8,17 +8,56 @@ class TestContactModel(TestCase):
         Contact.objects.create(
             first_name="Chris",
             last_name="Zichko",
-            email="test@email.com",
-            message="I'm adding a message to be sent to the administrator here.",
-            project_url="",
-            project_title="Test Project 1"
+            email="test@email.com"
         )
         
         Contact.objects.create(
             first_name="Chris",
             last_name="Zichko",
-            email="test@email.com",
-            message="I'm adding a message to be sent to the administrator here.",
-            project_url="https://zichkoding.com/project/test-project-1",
-            project_title=""
+            email="test2@email.com"
         )
+
+    def test_contact_model(self):
+        contact = Contact.objects.get(email="test2@email.com")
+        self.assertEqual(contact.first_name, "Chris")
+        self.assertEqual(contact.last_name, "Zichko")
+        self.assertEqual(contact.email, "test2@email.com")
+
+
+class TestMessageModel(TestCase):
+    def setUp(self):
+        self.contact = Contact.objects.create(
+            first_name="Chris",
+            last_name="Zichko",
+            email="test@email.com",
+        )
+        
+        self.contact_id = self.contact.id
+
+        Message.objects.create(
+            message="This is a test message",
+            project_url="http://www.test.com/test-project",
+            project_title="Test Project",
+            contact_id=self.contact
+        )
+
+        Message.objects.create(
+            message="This is a test message for test project 2",
+            project_url="http://www.test.com/test-project-2",
+            project_title="Test Project 2",
+            contact_id=self.contact
+        )
+
+    def test_message_model(self):
+        message = Message.objects.get(project_title="Test Project")
+        self.assertEqual(message.message, "This is a test message")
+        self.assertEqual(message.project_url, "http://www.test.com/test-project")
+        self.assertEqual(message.project_title, "Test Project")
+        self.assertEqual(message.contact_id.id, self.contact_id)
+
+        message2 = Message.objects.get(project_title="Test Project 2")
+        self.assertEqual(message2.message, "This is a test message for test project 2")
+        self.assertEqual(message2.project_url, "http://www.test.com/test-project-2")
+        self.assertEqual(message2.project_title, "Test Project 2")
+        self.assertEqual(message2.contact_id.id, self.contact_id)
+        
